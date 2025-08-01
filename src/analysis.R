b918dabfa1a37b10f0e4484235d4268478df3f57
ryan-bert <ryanberti66@gmail.com>
2025-08-01 21:06:18 +0200

    Calculate implied probabilities

--- src/analysis.R
+++ src/analysis.R
@@ -17,3 +17,52 @@ games_long_df <- read_csv(long_form_path, show_col_types = FALSE)
 
 ######################### IMPLIED PROBABILITIES #########################
 
+# Split into home and away games
+home_games_df <- games_long_df %>%
+  filter(Location == "Home")
+away_games_df <- games_long_df %>%
+  filter(Location == "Away")
+
+# Determine implied loss odds (prob. of opponent win)
+home_games_df <- home_games_df %>%
+  left_join(
+    away_games_df %>%
+      select(Match_ID, Win_Pinnacle_Odds, Win_Bet365_Odds) %>%
+      rename(
+        Lose_Pinnacle_Odds = Win_Pinnacle_Odds,
+        Lose_Bet365_Odds = Win_Bet365_Odds
+      ),
+    by = "Match_ID"
+  )
+away_games_df <- away_games_df %>%
+  left_join(
+    home_games_df %>%
+      select(Match_ID, Win_Pinnacle_Odds, Win_Bet365_Odds) %>%
+      rename(
+        Lose_Pinnacle_Odds = Win_Pinnacle_Odds,
+        Lose_Bet365_Odds = Win_Bet365_Odds
+      ),
+    by = "Match_ID"
+  )
+
+# Combine back into a single dataframe
+games_long_df <- bind_rows(home_games_df, away_games_df)
+
+# Determine implied draw odds
+games_long_df <- games_long_df %>%
+  left_join(
+    all_games_df %>%
+      select(Match_ID, Draw_Pinnacle_Odds, Draw_Bet365_Odds),
+    by = "Match_ID"
+  )
+
+# Calculate implied probabilities
+games_long_df <- games_long_df %>%
+  mutate(
+    Win_Pinnacle_Prob = 1 / Win_Pinnacle_Odds,
+    Win_Bet365_Prob = 1 / Win_Bet365_Odds,
+    Lose_Pinnacle_Prob = 1 / Lose_Pinnacle_Odds,
+    Lose_Bet365_Prob = 1 / Lose_Bet365_Odds,
+    Draw_Pinnacle_Prob = 1 / Draw_Pinnacle_Odds,
+    Draw_Bet365_Prob = 1 / Draw_Bet365_Odds
+  )
