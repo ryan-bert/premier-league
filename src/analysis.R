960f2eae0e95d86480ff61d9471ba2f464f67871
ryan-bert <ryanberti66@gmail.com>
2025-08-01 20:27:23 +0200

    basic initial structure

--- src/analysis.R
+++ src/analysis.R
@@ -6,8 +6,6 @@ suppressMessages({
   library(lubridate)
 })
 
-############################ DEFINE VARIABLES ############################
-
 # Define paths
 current_dir <- dirname(sys.frame(1)$ofile)
 all_games_path <- file.path(current_dir, "../data/clean_data/all_games.csv")
@@ -16,3 +14,6 @@ long_form_path <- file.path(current_dir, "../data/clean_data/long_form_games.csv
 # Read the combined data
 all_games_df <- read_csv(all_games_path, show_col_types = FALSE)
 games_long_df <- read_csv(long_form_path, show_col_types = FALSE)
+
+print(head(all_games_df))
+print(head(games_long_df))
